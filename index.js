5b6b25229d4ca1f6d731eff05fd9837eeea348ef
Ruth Chisom <ndukwe100805043@gmail.com>
2023-03-17 12:40:02 +0100

    Question 4

--- index.js
+++ index.js
@@ -9,6 +9,7 @@
 
 
 // Question 2: Here is an array of languages: [‘C#’, ‘JavaScript’, ‘Ruby’, ‘PHP’, ‘Python’]. Perform this operation on them 
+console.log("Question 2");
 const languages = ['C#', 'JavaScript', 'Ruby', 'PHP', 'Python'];
 console.log(languages);
 // 1. Add ‘Kotlin’ to the end of the array
@@ -41,13 +42,18 @@ fruit = ['apple', 'mango', 'orange'];
 
 // Question 4: Write a function that accepts an array of numbers as an argument. Return the maximum value in the array.
 // e.g., max([4, 5, 10, -2]) // maximum value is 10
-// const maximumValue = (arr) => Math.m
+
+console.log("Question 4");
+const maximumValue = (arr) => Math.max(...arr);
+const test = [4, 5, 10, -2];
+console.log(maximumValue(test));
 
 
 // Question 5: Write a function called valTimesIndex which accepts an array of numbers and returns a new array with each value multiplied by the index it is at in the array:
 // e.g.,
 //  valTimesIndex([1,2,3]) // [0,2,6]
 // 		valTimesIndex([5,10,15]) // [0,10,30]
+console.log("Question 5");
 const valTimesIndex = (arr) => {
     for(let key of arr){
         console.log(key);
@@ -55,6 +61,7 @@ const valTimesIndex = (arr) => {
 
     }
 }
-let values = [5,10,15];
+let values = [1,2,3];
+// let values = [5,10,15];
 console.log(valTimesIndex([values]));
 
